01af79539d31b2b54b659e2b0607daea9b5585f0
AMINE ABOU-ELOUAFA <abouelouafa.95@gmail.com>
2022-02-02 18:45:21 +0100

    First commit

---
+++ src/main/java/pack/com/SpringBootGestionDeCompteApplication.java
@@ -0,0 +1,13 @@
+package pack.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGestionDeCompteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootGestionDeCompteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pack/com/SpringBootGestionDeCompteApplicationTests.java
@@ -0,0 +1,13 @@
+package pack.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootGestionDeCompteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
